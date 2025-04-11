9d8403913a06c4d247ec34d7a451863d54df8eaa
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-04-11 20:01:22 -0300

    Update cnpj migration

--- src/database/migrations/migrations_scripts/20250411202737_add_CNPJ_column_to_companies.sql
+++ src/database/migrations/migrations_scripts/20250411202737_add_CNPJ_column_to_companies.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-ALTER TABLE companies ADD COLUMN CNPJ TEXT NOT NULL UNIQUE;
+ALTER TABLE companies ADD COLUMN cnpj TEXT NOT NULL UNIQUE;
 
 -- migrate:down
-ALTER TABLE companies DROP COLUMN CNPJ;
+ALTER TABLE companies DROP COLUMN cnpj;
