73593863e386652e4e72de37e325fba526d985b7
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-04-10 21:05:14 -0300

    Add create_companies_table migration

---
+++ src/database/migrations/migrations_scripts/20250410235924_create_companies_table.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS companies (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name TEXT NOT NULL,
+  create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS companies;
