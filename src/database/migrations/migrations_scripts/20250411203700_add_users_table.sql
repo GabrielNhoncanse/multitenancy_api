07b9d9629c5f4855d2eac078dc12fc7dfd849c8f
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-04-11 18:23:32 -0300

    Add users table

---
+++ src/database/migrations/migrations_scripts/20250411203700_add_users_table.sql
@@ -0,0 +1,13 @@
+-- migrate:up
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  company_id UUID NOT NULL REFERENCES companies(id) ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  role TEXT NOT NULL CHECK (role IN ('admin', 'manager', 'user')),
+  password_hash TEXT NOT NULL,
+  create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS users;
