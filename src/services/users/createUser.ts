843d790cf48ded324f4d1e9dd870668d5ae2b2d0
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-04-16 20:52:51 -0300

    Add createUser service

---
+++ src/services/users/createUser.ts
@@ -0,0 +1,26 @@
+import { CreateUserParams, CreateUserResult } from '../../types'
+import * as yup from 'yup'
+import bcrypt from 'bcrypt'
+import { UsersRepository } from '../../repositories'
+
+const newUserShape = yup.object().shape({
+  companyId: yup.string().uuid().required(),
+  name: yup.string().required(),
+  email: yup.string().email('The entered email is not valid').required(),
+  role: yup.string().required().oneOf(['admin', 'manager', 'user']),
+  plainPassword: yup.string().required()
+})
+
+const usersRepository = new UsersRepository()
+
+export async function createUser (
+  params: CreateUserParams
+): Promise<CreateUserResult> {
+  await newUserShape.validate(params)
+
+  const passwordHash = await bcrypt.hash(params.plainPassword, 13)
+
+  const newUserId = await usersRepository.create(params, passwordHash)
+
+  return newUserId
+}
