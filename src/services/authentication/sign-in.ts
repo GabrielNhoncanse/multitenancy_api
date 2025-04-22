c142721ec2e6f5143c787a02076cb76d2f9d2fea
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-04-21 23:27:49 -0300

    Add signIn service

---
+++ src/services/authentication/sign-in.ts
@@ -0,0 +1,36 @@
+import * as yup from 'yup'
+import bcrypt from 'bcrypt'
+import { SignInUserParams } from '../../types'
+import { UsersRepository } from '../../repositories'
+import jwt from 'jsonwebtoken'
+
+const signInSchema = yup.object().shape({
+  email: yup.string().email('The entered email is not valid').required(),
+  password: yup.string().required('Password is required')
+})
+
+const usersRepository = new UsersRepository()
+
+export async function signIn (
+  params: SignInUserParams
+): Promise<{ jwtToken: string }> {
+  await signInSchema.validate(params)
+
+  const { password } = params
+
+  const { companyId, userId, passwordHash } = await usersRepository.getByEmail(params.email)
+
+  const isValidPassword = await bcrypt.compare(password, passwordHash)
+
+  if (!isValidPassword) {
+    throw new Error('The given credentials are incorrect')
+  }
+
+  if (!process.env.JWT_SECRET_TOKEN) {
+    throw new Error('JWT_SECRET_TOKEN is not defined ')
+  }
+
+  const jwtToken = jwt.sign({ companyId, userId }, process.env.JWT_SECRET_TOKEN, { expiresIn: '1h' })
+
+  return { jwtToken }
+}
