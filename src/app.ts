1275693dda58bc88d80b15c1ecd98e174cd6581c
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-04-21 23:29:39 -0300

    Add authenticationRouter to app

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import express from 'express'
-import { companiesRouter, usersRouter } from './routers'
+import { authenticationRouter, companiesRouter, usersRouter } from './routers'
 
 export async function buildApp () {
   const app = express()
 
   app.use(express.json())
 
-  app.get('/health', (req, res) => {
-    res.send('Server is running')
-  })
+  app.get('/health', (_, res) => { res.status(200).send({ status: 'ok' }) })
 
+  // Unprotected routes
+  app.use('/auth', authenticationRouter)
   app.use('/companies', companiesRouter)
+
+  // Protected routes // to-do: add auth middleware
   app.use('/users', usersRouter)
 
   return app
