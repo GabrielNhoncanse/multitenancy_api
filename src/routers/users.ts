3e337de6c19b8809d8dc5b383438ee99917efbf4
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-04-23 20:17:10 -0300

    Add create user endpoint

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express'
+import { createUser } from '../services/users'
 
 export const usersRouter = Router()
 
-usersRouter.get('/', (req, res) => {
-  res.send('users')
+usersRouter.post('/', async (req, res) => {
+  try {
+    const { id: userId } = await createUser((req as any).user, req.body)
+
+    res.status(201).json({ userId })
+  } catch (error) {
+    res.status(400).json({ error: error instanceof Error ? error.message : 'Unkown error' })
+  }
 })
